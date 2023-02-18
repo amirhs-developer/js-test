b1c845b66cff199e515bff2a26707335cf53d22a
amirhossein <amir.askidev@gmail.com>
2023-02-18 12:10:18 +0330

    test setTimeOut and setInterval method and clear

--- scripts/BOM.js
+++ scripts/BOM.js
@@ -3,6 +3,71 @@
 window.parseFloat()
 window.parseInt()
 window.console.log()
+console.log(window.Infinity);
+
+function globalWindow(){
+    runzero = 10; //global 
+    window.f = () => { //global
+        console.log('f');
+    }
+}
+globalWindow();
+console.log(runzero);
+console.log(window.runzero);
+f();
+window.f();
+
+//? system dialogs 
+let alertDialogButton = document.getElementById('alert');
+let confirmDialogButton = document.getElementById('confirm');
+let promptDialogButton = document.getElementById('prompt');
+let printDialogButton = document.getElementById('print');
+
+alertDialogButton.addEventListener('click', () => {
+    alert('alert Dialog');
+})
+
+confirmDialogButton.addEventListener('click', () => {
+    let answer = confirm('are you sure ? ');
+    if(answer) alert('ok')
+    else alert('what ! please again');
+})
+
+promptDialogButton.addEventListener('click',() => {
+    let name = prompt('enter your name');
+    let family = prompt('enter your family','none');
+    let message = `your name is ${name} and your family is ${family}`
+    alert(message);
+})
+
+printDialogButton.addEventListener('click',() => print())
+
+document.addEventListener('beforeprint', () => {
+    printDialogButton.style.display = 'none';
+})
+
+document.addEventListener('afterprint', () => {
+    printDialogButton.style.display = 'none';
+})
+
+//? setTimeout and clearTimeOut
+//? setInterval and clearInterval
+
+
+let set = document.getElementById('set');
+let clear = document.getElementById('clear');
+let timer;
+set.addEventListener('click', () => {
+     timer = setInterval(()=>{
+        alert('ok');
+    },2000);
+})
+
+clear.addEventListener('click', () => {
+    clearInterval(timer);
+})
+
+
 
 
 
